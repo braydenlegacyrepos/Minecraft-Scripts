20ed80ac94d7ab02ee4650ecf50bd594179b15c7
Brayden Hull <brayden.hull@gmail.com>
2012-02-04 10:54:05 +0800

    Essentials latest version script thing.

---
+++ src/khobbits.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+LATEST_VER=`curl http://ess.khhq.net/build/build.php?build=bt2 | sed 's/\.//g'`
+CURRENT_VER=`cat ~/minecraft/.ess_ver`
+echo ${LATEST_VER}
+echo ${CURRENT_VER}
+if [ "${LATEST_VER}" -gt "${CURRENT_VER}" ]; then
+    screen -p 0 -S minecraft -X stuff "`printf "stop\r"`"
+    rm ~/minecraft/plugins/Essentials*.jar
+    wget http://ci.ess3.net/guestAuth/repository/download/bt2/.lastSuccessful/Essentials-full.zip
+    unzip Essentials-full.zip -d ~/minecraft/plugins/
+    screen -p 0 -S minecraft -X stuff "`printf "./minecraft.sh\r"`"
+    echo ${LATEST_VER} > ~/minecraft/.ess_ver
+    rm Essentials-full.zip
+fi
