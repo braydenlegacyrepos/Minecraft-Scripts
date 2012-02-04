94700f9caefcc073f35a9283eded8ac51ca5ca38
Brayden Hull <brayden.hull@gmail.com>
2012-02-04 10:56:25 +0800

    wait on shutdown

--- src/khobbits.sh
+++ src/khobbits.sh
@@ -3,6 +3,7 @@ LATEST_VER=`curl http://ess.khhq.net/build/build.php?build=bt2 | sed 's/\.//g'`
 CURRENT_VER=`cat ~/minecraft/.ess_ver`
 if [ "${LATEST_VER}" -gt "${CURRENT_VER}" ]; then
     screen -p 0 -S minecraft -X stuff "`printf "stop\r"`"
+    sleep 3
     rm ~/minecraft/plugins/Essentials*.jar
     wget http://ci.ess3.net/guestAuth/repository/download/bt2/.lastSuccessful/Essentials-full.zip
     unzip Essentials-full.zip -d ~/minecraft/plugins/
