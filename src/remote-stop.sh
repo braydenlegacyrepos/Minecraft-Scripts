5fda177130ef2eb2279a13774d2efc15b4d88d7e
Brayden Hull <brayden.hull@gmail.com>
2012-02-09 01:29:20 +0800

    Didn't this get committed?

--- src/remote-stop.sh
+++ src/remote-stop.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 while true; do
-NC_PACKET=`nc -l 112`
+NC_PACKET=`nc -l 112 -v`
 if [ "${NC_PACKET}" = "stop hping3" ]; then
     killall hping3
     echo "Received kill signal."
+else
+    echo ${NC_PACKET}
 fi
 done
