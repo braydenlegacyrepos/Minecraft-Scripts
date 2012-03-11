0798cddf5ba7e181a16543eca069a1d6b19cddb1
Brayden Hull <brayden.hull@gmail.com>
2012-03-12 01:17:13 +0800

    Done.. tested.. works.

--- src/remote-backup.sh
+++ src/remote-backup.sh
@@ -2,15 +2,15 @@
 BACKUP_USERNAME=root
 REMOTE_HOST=irc
 if [ "$1" = "Terraria" ]; then
-    TERRARIA_SRC=~/Terraria/
+    TERRARIA_SRC=~/Terraria/Worlds/
     TERRARIA_DST=/terraria-backup/
     echo "Terraria backup starting."
     rdiff-backup --print-statistics ${TERRARIA_SRC} ${BACKUP_USERNAME}@${REMOTE_HOST}::${TERRARIA_DST} >> ~/Terraria-backup.log
 elif [ "$1" = "Minecraft" ]; then
-    MINECRAFT_SRC=~/Minecraft/Main/
+    MINECRAFT_SRC=~/Minecraft/Main/world/
     MINECRAFT_DST=/mc-backup/
     echo "Minecraft backup starting."
-    rdiff-backup --print-statistics ${MINECRAFT_SRC} ${BACKUP_USERNAME}@${REMOTE_HOST}::${TERRARIA_DST} >> ~/Minecraft-backup.log
+    rdiff-backup --print-statistics ${MINECRAFT_SRC} ${BACKUP_USERNAME}@${REMOTE_HOST}::${MINECRAFT_DST} >> ~/Minecraft-backup.log
 else
-    echo -e "Accepted arguments are:\nMinecraft\nTerraria\nRev: 2"
+    echo -e "Accepted arguments are:\n$0 Minecraft\n$0 Terraria\nRev: 4 (final)"
 fi
