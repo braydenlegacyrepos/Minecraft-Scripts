14c00fd4e4000971cf5337b63e735015c5dfcad3
Brayden Hull <brayden.hull@gmail.com>
2012-03-12 00:24:37 +0800

    Oops.. forgot username.

--- src/remote-backup.sh
+++ src/remote-backup.sh
@@ -5,12 +5,12 @@ if [ "$1" = "Terraria" ]; then
     TERRARIA_SRC=~/Terraria/
     TERRARIA_DST=/terraria-backup/
     echo "Terraria backup starting."
-    rdiff-backup --print-statistics ${TERRARIA_SRC} ${REMOTE_HOST}::${TERRARIA_DST} >> ~/Terraria-backup.log
+    rdiff-backup --print-statistics ${TERRARIA_SRC} ${BACKUP_USERNAME}@${REMOTE_HOST}::${TERRARIA_DST} >> ~/Terraria-backup.log
 elif [ "$1" = "Minecraft" ]; then
     MINECRAFT_SRC=~/Minecraft/Main/
     MINECRAFT_DST=/mc-backup/
     echo "Minecraft backup starting."
-    rdiff-backup --print-statistics ${MINECRAFT_SRC} ${REMOTE_HOST}::${TERRARIA_DST} >> ~/Minecraft-backup.log
+    rdiff-backup --print-statistics ${MINECRAFT_SRC} ${BACKUP_USERNAME}@${REMOTE_HOST}::${TERRARIA_DST} >> ~/Minecraft-backup.log
 else
-    echo -e "Accepted arguments are:\nMinecraft\nTerraria\nRev: 1"
+    echo -e "Accepted arguments are:\nMinecraft\nTerraria\nRev: 2"
 fi
